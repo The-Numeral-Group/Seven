8b46cf0cbf5f160e1157721f8e87d4574068e5d4
ramgmtk <ramgmtk@gmail.com>
2021-01-06 13:48:52 -0800

    Added Camera Assets from Prototype

---
+++ Seven/Assets/Scripts/Camera/BossCamera.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//CREDIT FOR THE TEMPLATE CODE GOES TO BRACKEYS
+//https://www.youtube.com/watch?v=aLpixrPvlB
+public class BossCamera : MonoBehaviour
+{
+    public Transform playerTransform;
+    public List<GameObject> targets;
+    public float maxDistance = 50f;
+    public Vector2 offset = new Vector2(0, 2);
+    public float smoothTime = 0.5f;
+    private Vector3 velocity;
+    private Vector2 currOffset;
+
+    void FixedUpdate()
+    {
+        MoveCamera();
+    }
+    
+    /*
+    MoveCamera adjusts the cameras position.
+    By default it will focus on the player.
+    If there are targets close to the player, the camera will mediate a position between the player and all the targets.
+    The cameras movement is smoothed.
+    */
+    private void MoveCamera()
+    {
+        this.currOffset = this.offset;
+        Vector3 centerPoint = GetCenterPos();
+        Vector2 newPosition = new Vector2(centerPoint.x, centerPoint.y) + this.currOffset;
+        this.gameObject.transform.position = Vector3.SmoothDamp(this.gameObject.transform.position, new Vector3(newPosition.x, newPosition.y, this.gameObject.transform.position.z),
+                                                    ref this.velocity, this.smoothTime);
+    }
+
+    /*
+    GetCenterPos returns a position that is used to inform where the camera should move to.
+    That position by default is the players transform.
+    If there are targets close to the player the camera will go to position that gives view of the specified targets and the player.
+    This function is utilized by MoveCamera
+    */
+    private Vector3 GetCenterPos()
+    {
+        var bounds = new Bounds(this.playerTransform.position, Vector3.zero);
+        bounds.Encapsulate(this.playerTransform.position);
+        if (this.targets.Count > 0)
+        {
+            for (int i = 0; i < this.targets.Count; i++)
+            {
+                if (!targets[i].activeSelf)
+                {
+                    continue;
+                }
+                float targetDist = Vector2.Distance(new Vector2(this.playerTransform.position.x, this.playerTransform.position.y),
+                                                    new Vector2(this.targets[i].transform.position.x, this.targets[i].transform.position.y));
+                if (targetDist < this.maxDistance)
+                {
+                    bounds.Encapsulate(targets[i].transform.position);
+                    this.currOffset = Vector2.zero;
+                }
+            }
+        }
+        return bounds.center;
+    }
+}
---
+++ Seven/Assets/Scripts/Camera/ShakeCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CREDIT: https://www.youtube.com/watch?v=Y8nOgEpnnXo&t=171s&ab_channel=Brackeys
+// The following codes are from this video.
+[RequireComponent(typeof(Camera))]
+public class ShakeCamera : MonoBehaviour
+{
+    public Camera mainCam;
+    float shakeAmount = 0;
+    void Awake()
+    {
+        if (mainCam == null)
+        {
+            this.mainCam = Camera.main;
+        }
+    }
+
+    public void cameraShake(float amt, float length)
+    {
+        this.shakeAmount = amt;
+        InvokeRepeating("BeginShake", 0, 0.01f);
+        Invoke("StopShake", length);
+    }
+    void BeginShake()
+    {
+        if (this.shakeAmount > 0)
+        {
+            Vector3 camPos = mainCam.transform.position;
+
+            float offsetX = Random.value * this.shakeAmount * 2 - this.shakeAmount;
+            float offsetY = Random.value * this.shakeAmount * 2 - this.shakeAmount;
+
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            this.mainCam.transform.position = camPos;
+
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("BeginShake");
+        this.mainCam.transform.localPosition = Vector3.zero;
+    }
+}
