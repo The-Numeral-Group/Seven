6d355441783e7dfadf4f4e083dbb8e4ffe3f8aef
ramgmtk <ramgmtk@gmail.com>
2021-01-09 00:47:59 -0800

    Weapon scripts moved over from prototype. Weapon attack is an ability now

---
+++ Seven/Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage = 0;
+    void Start()
+    {
+        //set damage for individual hitboxes. This might vary depending on the weapon
+        //such as a sword that hits harder at the very tip
+        for(int childIndex = 0; childIndex < this.gameObject.transform.childCount; ++childIndex){
+            var childTransform = this.gameObject.transform.GetChild(childIndex);
+            childTransform.gameObject.GetComponent<WeaponHitbox>().damage = damage;
+        }  
+    }
+}
---
+++ Seven/Assets/Scripts/Player/WeaponHitbox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))] //I (Ram) do not know if it is correct to require the 2d collider.
+public class WeaponHitbox : MonoBehaviour
+{
+    public int damage { get; set; }
+    WeaponAbility wp;
+
+    void Start()
+    {
+        //Assuming the Hierarchy of objects is Actor->weapon->weaponhitbox
+        wp = this.transform.parent.transform.parent.gameObject.GetComponent<WeaponAbility>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (wp == null)
+        {
+            Debug.Log("Error: This WeaponHitbox is not the grandchild of an object with a WeaponAbility Script");
+            return;
+        }
+        if (wp.hitConnected) //Check to see
+        {
+            return;
+        }
+        wp.hitConnected = true;
+        //try to get the enemy's health object
+        var enemyHealth = collider.gameObject.GetComponent<ActorHealth>();
+        /*I (Ram) commented out the weakpoint code because I don't believe it is in the new repo.
+        //or a weakpoint if there's no regular health
+        if(enemyHealth == null){collider.gameObject.GetComponent<ActorWeakPoint>();}*/
+
+        //if the enemy can take damage (if it has an ActorHealth component),
+        //hurt them. Do nothing if they can't take damage.
+        if(enemyHealth != null){
+            enemyHealth.takeDamage(this.damage);
+        }
+    }
+}
