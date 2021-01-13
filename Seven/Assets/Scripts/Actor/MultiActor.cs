22643b60417d2d1fa373992d711e7bcc3ea3b3bf
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-01-12 20:41:44 -0800

    Finished MultiActor Implementation
    Has not been tested, but this MultiActor file gives us a uniform structure for handling multi-phase enemies/boss fights.
    
    Check the documentation in the drive (under Actor > MultiActor) for more info.

--- Seven/Assets/Scripts/Actor/MultiActor.cs
+++ Seven/Assets/Scripts/Actor/MultiActor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,29 +7,48 @@ public class MultiActor : MonoBehaviour
 {
     /*The list of every "phase" (that is, unique Actor objects)
     that this MultiActor uses. They are ordered by index, and will
-    be traversed by index unless manually jumped around.*/
-    [Tooltip("The list of every \"phase\" (that is, unique Actor objects) that this MultiActor uses. They are ordered by index, and will be traversed by index unless manually jumped around.")]
-    public List<GameObject> actorPhases;
+    be traversed by index unless manually jumped around.
+    
+    Actually just make people start them as transform children idk.*/
+    //[Tooltip("The list of every \"phase\" (that is, unique Actor objects) that this MultiActor uses. They are ordered by index, and will be traversed by index unless manually jumped around.")]
+    //public List<GameObject> actorPhases;
+    public List<GameObject> actorPhases{ get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        //Step 1: Instantiate every desired phase in an unactive state.
-        //  Give them a MATC so they can easily change their phase
-        foreach(GameObject actorPhase in actorPhases)
+        var myTransform = this.gameObject.transform;
+
+        this.actorPhases = new List<GameObject>();
+
+        //Step 0: If this object has an Actor parent, assume that parent is Phase 0
+        if(myTransform.parent != null && myTransform.parent.gameObject.GetComponent<Actor>() != null)
         {
-            actorPhase.SetActive(false);
-            var newPhase = Instantiate(actorPhase, this.gameObject.transform);
-            var matc = newPhase.AddComponent<MultiActorTransitionController>();
-            matc.hostMultiActor = this;
-            /*
-            we could do this line with...
-            Instantiate(actorPhase, this.gameObject.transform).AddComponent<MultiActorTransitionController>().hostMultiActor = this;
-            but that would be a massive pain
-            */
+            this.actorPhases.Add(myTransform.parent.gameObject);
+
+            //Step 0.5: give Phase 0 a MATC
+            myTransform.parent.gameObject.AddComponent<MultiActorTransitionController>().hostMultiActor = this;
         }
 
-        //
+        //Step 1: Add every phase to the actorPhases list
+        for(int i = 0; i < myTransform.childCount; ++i)
+        {
+            var addedChild = myTransform.GetChild(i).gameObject;
+            if(addedChild.GetComponent<Actor>() != null)
+            {
+                this.actorPhases.Add(addedChild);
+
+                //Step 1.3: give this phase a MATC
+                addedChild.AddComponent<MultiActorTransitionController>().hostMultiActor = this;
+
+                //Step 1.6: make sure this phase isn't on right now
+                addedChild.SetActive(false);
+            }
+        }
+
+        /*Step 2: If we're not already at Phase 0 (i.e. if the MultiActor's parent
+        isn't an actor or doesn't exist), go to Phase 0*/
+        if(this.gameObject.transform.parent == null){GoToPhase(0);}
     }
 
     // Update is called once per frame
@@ -37,18 +57,15 @@ public class MultiActor : MonoBehaviour
         
     }*/
 
-    /*Activates the next phase in the MultiActor's
-    transform (that is, Child 0)*/
-    public void NextPhase()
+    public Actor GoToPhase(int index)
     {
-        ChangePhase(0);
-    }
+        Transform myTransform = this.gameObject.transform;
+        Transform oldPhase = myTransform.parent;
 
-    public void ChangePhase(int index)
-    {
-        var myTransform = this.gameObject.transform;
-        var newPhase = myTransform.GetChild(index);
-        var oldPhase = myTransform.parent;
+        /*we don't know if the index actually exists, ?[] 
+        will make newPhase null if it doesn't. The ?. before
+        transform has a similar effect*/
+        Transform newPhase = actorPhases?[index]?.transform;
 
         //Check if oldPhase actually exists. Some phases
         //might destroy themselves when they end.
@@ -64,22 +81,69 @@ public class MultiActor : MonoBehaviour
             oldPhase.parent = myTransform;
         }
 
-        //de-child the new phase. It's now a free objecy
-        newPhase.parent = null;
+        //Check if newPhase actually exists. If it doesn, we can
+        //just stop here and return null.
+        if(newPhase != null)
+        {
+            //de-child the new phase. It's now a free object
+            newPhase.parent = null;
 
-        //make the MultiActor a child of newPhase. newPhase has now
-        //  replaced oldPhase
-        myTransform.parent = newPhase;
+            //make the MultiActor a child of newPhase. newPhase has now
+            //  replaced oldPhase
+            myTransform.parent = newPhase;
 
-        //activate newPhase. It is now the commanding gameObject.
-        //  Because phases are instantiated inactive, Awake will be
-        //  called at this time for every script on newPhase.
-        newPhase.gameObject.SetActive(true);
+            //activate newPhase. It is now the commanding gameObject.
+            //  Because phases are instantiated inactive, Awake will be
+            //  called at this time for every script on newPhase.
+            newPhase.gameObject.SetActive(true);
+
+            return newPhase.gameObject.GetComponent<Actor>();
+        }
+        else
+        {
+            return null;
+        }
 
     }
 }
 
 public class MultiActorTransitionController : MonoBehaviour
 {
-    public MultiActor hostMultiActor;
+    public MultiActor hostMultiActor = null;
+
+    /*Tuples let us pair arbitrary data together really easy-like. This lets us use
+    */
+    public void NextPhase(Tuple<Actor, Action<Actor>> args)
+    {
+        var lastPhaseIndex = hostMultiActor.actorPhases.IndexOf(args.Item1.gameObject);
+
+        var newPhase = hostMultiActor.GoToPhase(lastPhaseIndex + 1);
+
+        if(newPhase != null)
+        {
+            /*This version of Invoke is Action.Invoke, not ActorAbility.Invoke
+            Action is a subclass of the standard C# type Delegate*/
+            args.Item2?.Invoke(newPhase);
+        }
+        else
+        {
+            Debug.LogWarning("MultiActor attempted to shift to phase " + lastPhaseIndex + 1 + ", which doesn't exist");
+        }
+    }
+
+    public void ChangePhase(Tuple<int, Action<Actor>> args)
+    {
+        var newPhase = hostMultiActor.GoToPhase(args.Item1);
+
+        if(newPhase != null)
+        {
+            /*This version of Invoke is Action.Invoke, not ActorAbility.Invoke
+            Action is a subclass of the standard C# type Delegate*/
+            args.Item2?.Invoke(newPhase);
+        }
+        else
+        {
+            Debug.LogWarning("MultiActor attempted to shift to phase " + args.Item1 + ", which doesn't exist");
+        }
+    }
 }
