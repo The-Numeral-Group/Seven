2cd27e04d89551c83cc8c8eb895ed4d57a5dc4b5
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-01-15 15:18:40 -0800

    Created GluttonyAbilityInitiator and added Actor components to Gluttony Prefab
    GluttonyAbilityInitiator is super simple, it just writes some public ability references to the AbilityRegister and Gluttony's abilityDict.
    
    Neither have been tested, but that's how it be.

---
+++ Seven/Assets/Scripts/Bosses/Gluttony/GluttonyAbilityInitiator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GluttonyAbilityInitiator : ActorAbilityInitiator
+{
+    public ActorAbility Crush;
+    public ActorAbility Bite;
+    public ActorAbility PhaseZeroSpecial;
+    public ActorAbility Projectile;
+
+    void Awake()
+    {
+        //Manual initialization, 'cause I (Thomas) am lazy
+
+        this.abilities.Add("Gluttony" + nameof(Crush), Crush);
+        AbilityRegister.GLUTTONY_CRUSH = "Gluttony" + nameof(Crush);
+
+        this.abilities.Add("Gluttony" + nameof(Bite), Bite);
+        AbilityRegister.GLUTTONY_BITE = "Gluttony" + nameof(Crush);
+
+        this.abilities.Add("Gluttony" + nameof(PhaseZeroSpecial), PhaseZeroSpecial);
+        AbilityRegister.GLUTTONY_PHASEZERO_SPECIAL = "Gluttony" + nameof(PhaseZeroSpecial);
+
+        this.abilities.Add("Gluttony" + nameof(Projectile), Projectile);
+        AbilityRegister.GLUTTONY_PROJECTILE = "Gluttony" + nameof(Projectile);
+    }
+}
