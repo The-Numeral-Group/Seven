3630831fc300be0c4a0f9d4d37d6b0d61e5eedad
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-01-22 17:36:45 -0800

    Pride Ability: Shockwave
    Added a shockwave projectile ability, designed to be used by Pride.
    
    PrideShockwave is the Ability. It launches...
    PrideWaveProjectile is an BasicHitbox that moves and grows with time. It mirrors the cone-like behavior of the wave while also moving in a direction.
    
    Also created a few helper things:
    Renamed msic to Misc
    BasicHitbox is a box that just hurts anything that enters its trigger collider and that's it
    Rotation2D is a static class that holds rotation-related helper functions, mainly because most rotation functions in Unity don't map correctly to 2D spaces.
    
    I won't lie, I (Thomas) am mega stoopidbrain mode. This might be missing some stoff. But that's what testing is for!

---
+++ Seven/Assets/Scripts/Abilities/Pride/PrideShockwave.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*This class is just a simple ability wrapper/spawner for a projectile gameobject, which
+does the actual shockwaving*/
+public class PrideShockwave : ActorAbilityFunction<GameObject, int>
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    [Tooltip("The shockwave projectile that this ability launches (the effects of the shockwave" + 
+        " can be configured by that game object).")]
+    public PrideWaveProjectile waveProjectile;
+
+    [Tooltip("A target object to launch the shockwave at.")]
+    public GameObject target;
+
+    //A middleman variable to hold the wave projectile between methods
+    private GameObject waveObj;
+
+    //METHODS--------------------------------------------------------------------------------------
+    /*InternInvokes the ability as normal, but also instantiates a new waveObj to be launched by
+    InternInvoke.*/
+    public override void Invoke(ref Actor user)
+    {
+        //by default, Invoke just does InternInvoke with no arguments
+        if(usable)
+        {
+            isFinished = false;
+            waveObj = 
+                Instantiate(waveObj, user.gameObject.transform.position, Quaternion.identity);
+
+            InternInvoke(target);
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+        
+    }
+    /*Launch the projectile. The anticipated argument is the gameObject being shot at. The 
+    gameObject may-or-may-not have an ActorHealth component.*/
+    protected override int InternInvoke(params GameObject[] args)
+    {
+        waveObj.GetComponent<PrideWaveProjectile>().Launch(args[0].transform.position);
+        
+        isFinished = true;
+
+        return 0;
+    }
+}
---
+++ Seven/Assets/Scripts/Misc/BasicHitbox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Dead-basic hitbox. It's a box that hits you. Activating it and
+reseting it for a second attack is the user's problem*/
+[RequireComponent(typeof(Collider2D))]
+public class BasicHitbox : MonoBehaviour
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    [Tooltip("How much damage this hitbox should cause.")]
+    public float damage;
+
+    [Tooltip("Whether or not this hitbox should be ignored due to already striking something.")]
+    public bool hitAlreadyLanded = false;
+
+    //METHODS--------------------------------------------------------------------------------------
+    void OnTriggerEnter2D(Collider2D collided)
+    {
+        if(!hitAlreadyLanded)
+        {
+            var health = collided.gameObject.GetComponent<ActorHealth>();
+
+            if(health)
+            {
+                health.takeDamage(damage);
+                hitAlreadyLanded = true;
+            }
+        }
+    }
+}
