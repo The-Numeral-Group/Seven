fcc15112242335f1940ad3b250c881573238ce79
ramgmtk <ramgmtk@gmail.com>
2021-02-03 15:08:16 -0800

    Created base mnu class and menu manager for ui management and global calls

---
+++ Seven/Assets/Scripts/UI/DialogueMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class DialogueMenu : BaseUI
+{
+    //Reference to yarnspinners dialogue runner. Expected to be set through Inspector
+    public DialogueRunner dialogueRunner;
+    //Set the speakerNameTextBox. Expected to be set through Inspector.
+    public Text speakerNameTextBox;
+}
---
+++ Seven/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*This class serves as a container for the the various menus present in the game.
+I (Ram) created this as a means to access various menu elements from other classes globally.*/
+public class MenuManager : MonoBehaviour
+{
+    //Reference to the dialoguemenu. Reference expected to be set through inspector
+    [SerializeField]
+    [Tooltip("Reference to the dialoguemenu object.")]
+    DialogueMenu dialogueMenu;
+    //static reference to the Dialogue menu;
+    public static DialogueMenu DIALOGUE_MENU;
+
+    //Set static members to the inspector references
+    void Start()
+    {
+        DIALOGUE_MENU = dialogueMenu;
+        if (!DIALOGUE_MENU)
+        {
+            Debug.LogWarning("MenuManager: DialogMenu not hooked up properly.");
+        }
+    }
+}
