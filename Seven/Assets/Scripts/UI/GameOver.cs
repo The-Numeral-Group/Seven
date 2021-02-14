9ac5aa276ed51f00cf489e8044068ee8e64e6d52
ramgmtk <ramgmtk@gmail.com>
2021-02-14 02:26:54 -0800

    Changes made across the board to ensure keyboard selection through ui menus

--- Seven/Assets/Scripts/UI/GameOver.cs
+++ Seven/Assets/Scripts/UI/GameOver.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOver : BaseUI
 {
+    //references to both buttons should be set via inspector
+    [Tooltip("Reference to this ui objects restart button.")]
+    public Button restartButton;
+    [Tooltip("Reference to this ui objects exit button.")]
+    public Button exitButton;
+
     public void CloseApplication()
     {
         Application.Quit();
@@ -11,5 +18,11 @@ public class GameOver : BaseUI
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        restartButton.Select();
     }
 }
--- Seven/Assets/Scripts/UI/PauseMenu.cs
+++ Seven/Assets/Scripts/UI/PauseMenu.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 //Expected to be attached to the pause menu ui.
 public class PauseMenu : BaseUI
 {
     //Flag for if the game is paused
     public static bool GAME_IS_PAUSED;
+    //exitButton should be set through inspector;
+    public Button exitButton;
 
     //Public Function which will pause the game
     public void PauseGame()
@@ -40,5 +41,11 @@ public class PauseMenu : BaseUI
         GAME_IS_PAUSED = false;
         Time.timeScale = 1f;
         base.Hide();
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        exitButton.Select();
     }
 }
