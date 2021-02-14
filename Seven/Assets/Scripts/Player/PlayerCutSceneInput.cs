bfd1c712ab9982f7983f493cfc6dcc14548ff08a
Mingun Cho <mingun0108@gmail.com>
2021-02-13 19:13:17 -0800

    Cutscene of interaction between ghost knight and player is now completed.

---
+++ Seven/Assets/Scripts/Player/PlayerCutSceneInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayerCutSceneInput : MonoBehaviour
+{
+    public GameObject timeline;
+    TimelineManager timelineManager;
+    void Start()
+    {
+        this.timelineManager = timeline.GetComponent<TimelineManager>();
+        if(timeline == null)
+        {
+            Debug.Log("PlayerCutSceneInput: Timeline Object is missing!");
+        }
+        if(timelineManager == null)
+        {
+            Debug.Log("PlayerCutSceneInput: Cannot find timelineManager componenet!");
+        }
+    }
+    void Update()
+    {
+        if (timeline == null)
+        {
+            Debug.Log("PlayerCutSceneInput: Timeline Object is missing!");
+        }
+        if (timelineManager == null)
+        {
+            Debug.Log("PlayerCutSceneInput: Cannot find timelineManager componenet!");
+        }
+    }
+
+    void OnDodge()
+    {
+        timelineManager.resumeTimeline();
+    }
+}
