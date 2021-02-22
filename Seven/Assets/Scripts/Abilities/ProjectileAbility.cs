4314da77efc986d2c619ce1fa4983a179f1dd303
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-02-22 12:23:33 -0800

    Basic Projectile Scripts
    A very general-use projectile script to use for whatever projectile-like behaviours we might desire, as well as a ProjectileAbility wrapper to make using with the Ability system very easy.

---
+++ Seven/Assets/Scripts/Abilities/ProjectileAbility.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAbility : ActorAbilityFunction<Vector2, int>
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    [Tooltip("The projectile that this ability launches (the effects of the projectile" + 
+        " can be configured by that game object).")]
+    public GameObject projectile;
+
+    [Tooltip("Where the projectile should spawn relative to the user's faceAnchor.")]
+    public Vector2 projectileScale = new Vector2(1, 1);
+
+    [Tooltip("Which direction the ability should be launched in if the user doesn't" + 
+        " specify a Transform, Vector2 direction, Actor, or gameObject target")]
+    public Vector2 projectileDirection = Vector2.left;
+
+    //A middleman variable to hold the projectile instance between methods
+    private GameObject projObj;
+
+    //A middleman variable to hold the desired LAUNCH_MODE between methods
+    private LAUNCH_MODE launchMode = LAUNCH_MODE.DIRECTION;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(projectile.GetComponent<BasicProjectile>() == null)
+        {
+            Debug.LogError("ProjectileAbility: the provided projectile gameObject" + 
+                " does not have a BasicProjectile component.");
+        }
+    }
+
+    //METHODS--------------------------------------------------------------------------------------
+    /*InternInvokes the ability as normal, but also instantiates a new projObj to be launched by
+    InternInvoke.*/
+    public override void Invoke(ref Actor user)
+    {
+        if(usable)
+        {
+            isFinished = false;
+            projObj = InstantiateProjectile(projectile, user.faceAnchor, projectileScale);
+
+            InternInvoke(projectileDirection);
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+        
+    }
+
+    public override void Invoke(ref Actor user, params object[] args)
+    {
+        if(usable)
+        {
+            isFinished = false;
+            projObj = InstantiateProjectile(projectile, user.faceAnchor, projectileScale);
+            launchMode = ConvertMode(args[1]);
+
+            InternInvoke(ConvertTarget(args[0]));
+            
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+        
+    }
+
+    /*Launch the projectile. The anticipated argument is the gameObject being shot at. The 
+    gameObject may-or-may-not have an ActorHealth component.*/
+    protected override int InternInvoke(params Vector2[] args)
+    {
+        projObj.GetComponent<BasicProjectile>().Launch(args[0], launchMode);
+
+        isFinished = true;
+
+        return 0;
+    }
+
+    /*Creates the projectile object relative to the faceAnchor to make adjusting its spawn position
+    with the inspector easier. Projectiles will still end and be launched parentless however,
+    as their movement methods assume that projectiles have no parents.*/
+    GameObject InstantiateProjectile(GameObject projPrefab, Transform faceAnchor, Vector2 offset)
+    {
+        //Instantiate the projectile as a child of faceAnchor
+        GameObject obj = Instantiate(projPrefab, faceAnchor);
+
+        //make it a child of faceAnchor's parent
+        obj.transform.parent = faceAnchor.parent;
+
+        //adjust projectile's position to match the faceAnchor's (with offset)
+        obj.transform.localPosition = faceAnchor.localPosition * offset;
+
+        //deparent the projectile
+        obj.transform.parent = null;
+        
+        //return the projectile
+        return obj;
+    }
+
+    Vector2 ConvertTarget(object arg)
+    {
+        Vector2 finalVec = projectileDirection;
+
+        else if(arg is Vector2)
+        {
+            finalVec = (Vector2)arg;
+        }
+        else if(arg is Transform)
+        {
+            finalVec = (arg as Transform).position;
+        }
+        else if(arg is GameObject)
+        {
+            finalVec = (arg as GameObject).transform.position;
+        }
+        else if(arg is Actor)
+        {
+            finalVec = (arg as Actor).gameObject.transform.position;
+        }
+        else
+        {
+            Debug.LogWarning("ProjectileAbility: provided argument cannot be" + 
+                " converted/interpreted to Vector2. Using projectileDirection instead.");
+        }
+        
+        return finalVec;
+    }
+
+    LAUNCH_MODE ConvertMode(object mode)
+    {
+        LAUNCH_MODE finalMode = LAUNCH_MODE.DIRECTION;
+
+        if(mode is LAUNCH_MODE)
+        {
+            finalMode = (LAUNCH_MODE)mode;
+        }
+        else
+        {
+            Debug.LogWarning("ProjectileAbility: provided argument cannot be" + 
+                " converted/interpreted to LAUNCH_MODE. Using LAUNCH_MODE.POINT instead.");
+        }
+
+        return finalMode;
+    }
+}
---
+++ Seven/Assets/Scripts/Misc/BasicProjectile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+public enum LAUNCH_MODE
+{
+    POINT,
+    DIRECTION
+}
+
+[RequireComponent(typeof(ActorMovement))]
+[RequireComponent(typeof(Collider2D))]
+public class BasicProjectile : MonoBehaviour
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    [Tooltip("How much damage this hitbox should cause.")]
+    public float damage;
+
+    [Tooltip("Whether or not the projectile should stop existing if it hits something.")]
+    public bool destroyOnHit = true;
+
+    //The function that will be called when the rubble starts moving.
+    protected Action<Vector2> moveFunction = null;
+
+    //The direction the rubble will be going;
+    private Vector2 launchDirection = Vector2.zero;
+
+    //The rubble's actor movement, which does its actual movement
+    private ActorMovement mover;
+
+    //METHODS--------------------------------------------------------------------------------------
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the ActorMovement for moving later
+        mover = this.gameObject.GetComponent<ActorMovement>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveFunction?.Invoke(launchDirection);
+    }
+
+    //What happens when the projectile actually hits something
+    protected virtual void OnTriggerEnter2D(Collider2D collided)
+    {
+        //we just want the normal hitbox method
+        var health = collided.gameObject.GetComponent<ActorHealth>();
+
+        if(health)
+        {
+            health.takeDamage(damage);
+        }
+
+        if(destroyOnHit)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    /*What should happen every time the projectile moves (including the movement)*/
+    protected virtual void InternalMovement(Vector2 movementDirection)
+    {
+        //first, move the rubble
+        mover.MoveActor(movementDirection);
+    }
+
+    /*Starts the projectile!*/
+    public void Launch(Vector2 target, LAUNCH_MODE mode = LAUNCH_MODE.POINT)
+    {
+        /*An explicit cast is added here, even though Vector3 implicitly converts
+        to Vector2, to remove the ambiguity between subtracting the this.gameObject's position as
+        a Vector3 or a Vector2*/
+        if(mode == LAUNCH_MODE.POINT)
+        {
+            launchDirection = (target - (Vector2)this.gameObject.transform.position).normalized;
+        }
+        else
+        {
+            launchDirection = target.normalized;
+        }
+        
+        //S H M O O V E
+        moveFunction = new Action<Vector2>(InternalMovement);
+    }
+}
