15c2e5e527e4eecdcf4a72028380ec6a455e0f28
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-02-25 15:56:49 -0800

    TimeWarp Draft
    Added several classes to assist with safely handling the Time Warp effect of Sloth, as well as its multi-switch activation mechanism.
    
    Also moved SimpleController2D to the Misc folder and removed the DevelopmentUtilities folder

--- Seven/Assets/Scripts/Actor Effects/SlothClockTimeWarp.cs
+++ Seven/Assets/Scripts/Actor Effects/SlothClockTimeWarp.cs
@@ -2,38 +2,137 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlothClockTimeWarp : MonoBehaviour, ActorEffect
+public class SlothClockTimeWarp : MonoBehaviour
 {
     //FIELDS---------------------------------------------------------------------------------------
+    [Tooltip("All of the objects that are considered when activating a timewarp")]
+    public List<GameObject> colliderObjects;
+
     [Tooltip("By what percentage timescale should increase when time is warped (A value of 1" + 
         " will increase timescale by 100%).")]
     public float timeFactor = 0.25f;
 
-    //The amount of these effects that can ever exist at once
-    public const float effectMax = 3;
+    [Tooltip("An arbitrary ActorEffectHandler to bear the slowdown effects.")]
+    public ActorEffectHandler handler;
+
+    //The lowest the timescale can be driven by this effect
+    public const float effectmin = 0.1f;
 
     //The amount of these effects that currently exist
-    public static float effectAmount = 0;
+    //public static float effectAmount = 0;
 
     //The discreet value by which timeScale will actually be changed
     private float timeAdjustment;
+
+    //a second list which tracks which gameObjects have or have not been hit
+    private List<GameObject> trackedObjects;
     //METHODS--------------------------------------------------------------------------------------
     // Start is called before the first frame update
     void Start()
     {
+        //calculate timeAdjustment
         timeAdjustment = Time.timeScale * timeFactor;
+
+        //add an observer to all of the added objects
+        foreach(GameObject obj in colliderObjects)
+        {
+            AddObserverComponent(obj);
+        }
+
+        //create the tracker list
+        trackedObjects = new List<GameObject>(colliderObjects);
     }
 
-    void onTriggerEnter2D(Collider2D collided){
+    /*adds an observer component to the gameobject, allowing this script to read
+    events off of it without the gameobject needing a reference at compiletime.*/
+    void AddObserverComponent(GameObject obj)
+    {
+        //Step 1: create the unity event to watch for
+        var ue = new ObjectSelfEditEvent();
 
+        //Step 2: set up UpdateClock to trigger when the event is invoked
+        ue.AddListener(UpdateClock);
+
+        //Step 3: create a new observer component
+        //Step 4: and give the component the new event
+        obj.AddComponent<SlothClockObserver>().responseEvent = ue;
+
+        //Step 5: assume the object has a collider component that can listen to attacks
     }
 
-    public bool ApplyEffect(ref Actor actor){
-        return false;
+    /*updates the clock's tracker list. If this results in the list being empty, then
+    reset the lit and apply the time effect*/
+    void UpdateClock(GameObject obj)
+    {
+        trackedObjects.Remove(obj);
+
+        //if the list is empty...
+        if(trackedObjects.Count == 0)
+        {
+            //reset the list and apply the timewarp
+            trackedObjects = new List<GameObject>(colliderObjects);
+            handler.AddEffect(new SlothClockTimeWarp.TimeWarpEffect(timeAdjustment));
+        }
     }
 
-    public void RemoveEffect(ref Actor actor){
+    /*WHAT THE SHIT IT'S A WHOLE DIFFERENT CLASS DECLARATION
+    Yes! To allow for individual instances of time changes to have different values,
+    a private class is created to be able to handle each effect*/
+    class TimeWarpEffect : ActorEffect 
+    {
+        //FIELDS-----------------------------------------------------------------------------------
+        //The actual value that got added to the timescale by this class instance
+        private float timeChange = 0;
 
+        //METHODS----------------------------------------------------------------------------------
+        //Sets the amount we actually want to change time by
+        public TimeWarpEffect(float timeChange)
+        {
+            this.timeChange = timeChange;
+        }
+
+        //LETS DO THE TIMEWARP AGAIN
+        public bool ApplyEffect(ref Actor actor)
+        {
+            /*first, check if doing this application would underball the minimum
+            timescale. If it would, abort the change*/
+            if(Time.timeScale - timeChange < SlothClockTimeWarp.effectmin)
+            {
+                Debug.LogWarning($"TimeWarpEffect in SlothClockTimeWarp: attempted timescale change" + 
+                    " from {Time.timeScale} to {Time.timeScale} goes under the minimum of" + 
+                        " {SlothClockTimeWarp.effectmin}.");
+
+                return false;
+            }
+            else
+            {
+                Time.timeScale += timeChange;
+            }
+
+            return true;
+        }
+
+        //undoes the timescale effect
+        public void RemoveEffect(ref Actor actor)
+        {
+            Time.timeScale -= timeChange;
+        }
+    }
+}
+
+class SlothClockObserver : MonoBehaviour
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    //The event that gets fired by this observer
+    [System.NonSerialized]
+    public ObjectSelfEditEvent responseEvent = null;
+
+    //might want to set this up a little differently, as OTE2D may also
+    //respond to entry from other objects, like things walking into it?
+    //FIELDS---------------------------------------------------------------------------------------
+    void OnTriggerEnter2D()
+    {
+        responseEvent.Invoke(this.gameObject);
     }
 }
 
---
+++ Seven/Assets/Scripts/Misc/ObjectSelfEditEvent.cs
@@ -0,0 +1,7 @@
+﻿/*CLASS WITH NO DATA
+but seriously this class is just a standard unity event, except
+it takes 1 argument of type GameObject in its Invoke function.
+This is definied as the generic part of UnityEvent, so no code
+is needed here, except for the class declaration*/
+
+public class ObjectSelfEditEvent : UnityEngine.Events.UnityEvent<UnityEngine.GameObject> {}
