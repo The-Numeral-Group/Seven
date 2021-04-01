8b5b66fb274f816d1d4391a497afb87466f05e27
Mingun Cho <mingun0108@gmail.com>
2021-03-31 17:53:38 -0700

    Updated Game Save Manager Documents and Audio Documents. Updated Player Attack animation

--- Seven/Assets/Scripts/System/GameSave/Position/SavePosition.cs
+++ Seven/Assets/Scripts/System/GameSave/Position/SavePosition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Doc: https://docs.google.com/document/d/1D2_wIAB-If-33qhKYqsUUPh2Jfy8WRf7MuSL1LFJYYo/edit
 public class SavePosition : MonoBehaviour
 {
     public List<GameObject> GameObjects = new List<GameObject>();
