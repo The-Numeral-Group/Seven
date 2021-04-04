816a1434caa8027c3f0c292a33e30f4de7d843a0
Mingun Cho <mingun0108@gmail.com>
2021-04-03 17:21:11 -0700

    Added Ghost Knight's attack tutorial text

---
+++ Seven/Assets/Scripts/UI/FollowObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reference: https://www.youtube.com/watch?v=7XVSLpo97k0&ab_channel=Epitome
+public class FollowObject : MonoBehaviour
+{
+    public Transform lookAt;
+    public Vector3 offset;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = cam.WorldToScreenPoint(lookAt.position + offset);
+
+        if(transform.position != pos)
+        {
+            transform.position = pos;
+        }
+    }
+}
