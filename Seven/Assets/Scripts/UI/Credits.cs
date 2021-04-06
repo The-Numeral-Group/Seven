854fbd8b81366f4d82323243822d70457b100aac
ramgmtk <ramgmtk@gmail.com>
2021-04-05 22:59:39 -0700

    Added credits sequence

---
+++ Seven/Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//This class is used as a contianer of callback functions to be used by the credit sequence.
+public class Credits : MonoBehaviour
+{
+    public MainMenu mainMenu;
+    public GameObject startMenu;
+    public Button startMenuDefaultButton;
+    public GameObject creditsContainer;
+
+    public void CreditsFinishSequence()
+    {
+        creditsContainer.SetActive(false);
+        mainMenu.startingButton = startMenuDefaultButton;
+        mainMenu.ShowSubMenu(startMenu);
+    }
+}
