6a0a5bc56064bb649e2de09388696d5d71fcaa8f
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-04-09 16:08:41 -0700

    Ego Initialization and Teleport Draft
    >Added Ego Scene
    >Added Ego2Movement, Phase 2 Ego's ActorMovement which does the teleportation
    >cleaned up case mis-match in scripts folder

---
+++ Seven/Assets/Scripts/Bosses/Ego/Ego2Actor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Ego's Second Phase is so different from its first that it requires two seperate Actor
+classes to function.*/
+
+public class Ego2Actor : Actor
+{
+    /*// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
