49800b555272f5727657aa03789f1a5ad2ab7aeb
ramgmtk <ramgmtk@gmail.com>
2021-04-13 18:08:12 -0700

    Helper script that is a container for a ui objects ui elements

---
+++ Seven/Assets/Scripts/UI/SubMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*Class is meant to be attached to ui objects that serve as sub menus within a larger menu ui object.
+Primary use is to serve as a container for values*/
+public class SubMenu : MonoBehaviour
+{
+    [Tooltip("The name of this sub menu")]
+    public string menuName;
+    [Tooltip("The defauly button that should be selected when this menu is active.")]
+    public Button defaultButton;
+}
