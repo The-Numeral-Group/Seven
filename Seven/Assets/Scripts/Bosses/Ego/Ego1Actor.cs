fa345966f600ed1cff01ba8c1284e8f5aeab5f56
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-04-15 10:27:01 -0700

    Ego1 Actor Draft
    Basically copied PrideActor and ripped out all of Pride's flavor bits. Ego might gain some of its own later.

---
+++ Seven/Assets/Scripts/Bosses/Ego/Ego1Actor.cs
@@ -0,0 +1,276 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*This was copied almost wholesale from PrideActor. Some comments may still reference Pride or
+its attacks, such as how the far charge is sometimes called a wave projectile, because that's
+what went in that slot of Pride originally.*/
+
+public class Ego1Actor : Actor
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    [Header("Attacks")]
+    [Tooltip("Controls how many times should Pride use normal attacks before using it's special.")]
+    public int specialAttackGate = 7;
+
+    [Tooltip("How far away the player should be before Ego charges at them.")]
+    public float chargeRange = 60f;
+
+    [Tooltip("How far away the player should be before Pride launches" + 
+        " a short-range attack at them.")]
+    public float punchRange = 25f;
+
+    [Header("Other")]
+    [Tooltip("What Pride is currently doing. Change this to make Pride do something.")]
+    public State currentState;
+
+    //All of Pride's possible states.
+    public enum State
+    {
+        WALK,
+        PHYSICAL_PUNCH,
+        PHYSICAL_SHOCKWAVE,
+        PHASE0_SPECIAL,
+        NULL,
+    }
+
+    /*reference to this script. Abilities need a reference to their user to be used, but 'this'
+    is read-only, and thus can't used. We need to manually reobtain a writable reference.*/
+    private Actor pride;
+
+    //reference to player for movement tracking and convinience
+    private Actor player;
+
+    //whatever ability Pride is currently using, if any
+    private ActorAbility currAbility;
+
+    //internal counter to track when Pride should use its special
+    private int specialAttackCounter = 0;
+
+    //METHODS--------------------------------------------------------------------------------------
+    /*Aquires references to needed actor components, then sets Pride's speed based on
+    overrideDefaultSpeed and speedModifer*/
+    new void Start()
+    {
+        //get component references
+        base.Start();
+
+        //find player...
+        var playerObject = GameObject.FindGameObjectsWithTag("Player")?[0];
+
+        //and save it's actor, if possible
+        if(playerObject == null)
+        {
+            Debug.LogWarning("Ego1Actor: Ego can't find the player!");
+        }
+        else
+        {
+            player = playerObject.GetComponent<Actor>();
+        }
+
+        //save pride's own actor
+        pride = this.gameObject.GetComponent<PrideActor>();
+    }
+
+    // FixedUpdate is called once every 60th of a second, regardless of framerate
+    void FixedUpdate()
+    {
+        ///DEBUG
+        //currentState = State.WALK;
+        Debug.Log("Pride State: " + currentState);
+        ///END DEBUG
+        EvaluateState(currentState);
+    }
+
+    /*Pride's state machine. Every time this is called, Pride choses a behvaior based on the
+    world around it, ususally its distance to the player. Check each case for full 
+    explanation.
+    
+    And yes, I (Thomas) ripped this from Gluttony because Gluttony and Pride act the same way.
+    I (Thomas) feel there's a better way to do ability activity tracking than this, but I don't
+    know what it is, so I'll use Ram's method (currAbility checking) for now.*/
+    void EvaluateState(State state)
+    {
+        switch (state)
+        {
+            //Moves Pride towards the player and decides what it should be doing right now
+            case State.WALK:
+                //Check if Pride is busy doing an ability, and thus shouldn't act
+                if (currAbility && !currAbility.getUsable())
+                {
+                    this.myMovement.MoveActor(new Vector2(0, 0));
+                    break;
+                }
+
+                //Check for special attack. Once it's high enough, Pride uses its special.
+                if (specialAttackCounter >= specialAttackGate)
+                {
+                    specialAttackCounter = 0;
+                    currentState = State.PHASE0_SPECIAL;
+                }
+                //If it's not special time, Pride will take a step, then decide what to do next.
+                else
+                {
+                    stepTowardsPlayer();
+                    currentState = decideNextState();
+                }
+
+                break;
+
+            //Activates Pride's long-range Shockwave Smash or Shattered Visage
+            case State.PHYSICAL_SHOCKWAVE:
+                var wave = this.myAbilityInitiator.abilities[AbilityRegister.PRIDE_FAR_ATTACK];
+
+                if(wave.getUsable())
+                {
+                    currAbility = wave;
+                    wave.Invoke(ref pride, player);
+                    specialAttackCounter++;
+                }
+
+                currentState = State.WALK;
+                break;
+
+            //Activates Pride's short-range Big-Ass Punch or Scorned Punch
+            case State.PHYSICAL_PUNCH:
+                var punch = this.myAbilityInitiator.abilities[AbilityRegister.PRIDE_CLOSE_ATTACK];
+
+                if(punch.getUsable())
+                {
+                    currAbility = punch;
+                    punch.Invoke(ref pride, player);
+                    specialAttackCounter++;
+                }
+
+                currentState = State.WALK;
+                break;
+
+            //Activates Pride's Special, Vanity Tour or Desperate Assault
+            case State.PHASE0_SPECIAL:
+                var special = this.myAbilityInitiator.abilities[AbilityRegister.PRIDE_SPECIAL];
+
+                if(special.getUsable())
+                {
+                    currAbility = special;
+                    special.Invoke(ref pride, player);
+                }
+
+                currentState = State.WALK;
+
+                break;
+
+            case State.NULL:
+                break;
+
+            default:
+                Debug.LogWarning("Ego1Actor: Ego has fallen out of its state machine!");
+                break;
+        }
+    }
+
+    /*Calculates where the player is, then makes one movement instance towards them.
+    
+    Yes, this was copied from Gluttony.*/
+    void stepTowardsPlayer()
+    {
+        var myPos = this.gameObject.transform.position;
+        var playerPos = player.gameObject.transform.position;
+
+        /*Fun Fact! Because positions are vectors, the normalized difference between
+        posB (myPos) and posA (playerPos) is the direction from posA to posB.*/
+        var directionToPlayer = (playerPos - myPos).normalized;
+
+        ///DEBUG
+        //Debug.Log("Pride moving in this direction: " + directionToPlayer);
+        ///DEBUG
+
+        this.myMovement.MoveActor(directionToPlayer);
+    }
+
+    /*Returns a state to enter based on ability activation conditions, all of which are judged by
+    player distance.
+    
+    Yes, this was copied from Gluttony.*/
+    State decideNextState()
+    {
+        var distanceToPlayer 
+            = Vector2.Distance(player.transform.position, this.gameObject.transform.position);
+        var punchReady 
+            = this.myAbilityInitiator.abilities[AbilityRegister.PRIDE_CLOSE_ATTACK].getUsable();
+        var projReady
+            = this.myAbilityInitiator.abilities[AbilityRegister.PRIDE_FAR_ATTACK].getUsable();
+        ///DEBUG
+        //Debug.Log("Dist to player: " + distanceToPlayer);
+        ///DEBUG
+        State nextState;  
+
+        if(distanceToPlayer >= chargeRange && projReady)
+        {
+            nextState = State.PHYSICAL_SHOCKWAVE;
+        }
+        else if(distanceToPlayer <= punchRange && punchReady)
+        {
+            nextState = State.PHYSICAL_PUNCH;
+        }
+        else
+        {
+            nextState = State.WALK;
+        }
+
+        return nextState;
+        
+    }
+
+    /*Initializes the next phase of the fight. I (Thomas) don't think Ego2 needs any
+    info passed into it from Ego1, but I'm going to leave this here, just in case...*/
+    void InitializePhase(Actor actor)
+    {
+        /*//getting actor components
+        PrideActor startingPhase = this;
+        PrideActor newPhase;
+        if(actor is PrideActor)
+        {
+            newPhase = actor as PrideActor;
+        }
+        else
+        {
+            Debug.LogError("PrideActor: attempted to change phase to something that isn't Pride");
+            return;
+        }
+
+        //assign weakpoints to new phase
+        newPhase.weakSpots = startingPhase.weakSpots;
+
+        //high count is arbitrary, it just needs to be high to prevent unintended phase change
+        newPhase.weakSpotGate = newPhase.weakSpots.Count * 10;
+
+        //assign new phase to weakpoints
+        foreach (ActorWeakPoint weakSpot in newPhase.weakSpots)
+        {
+            weakSpot.ownerHealth = newPhase.myHealth;
+        }
+
+        //match the old phase's health
+        newPhase.myHealth.currentHealth = startingPhase.myHealth.currentHealth;
+
+        //match the old phase's size
+        newPhase.gameObject.transform.localScale = startingPhase.gameObject.transform.localScale;
+
+        //and I think that's everything...*/
+    }
+
+    //Ego1 will switch to Ego2 upon death.
+    public override void DoActorDeath()
+    {
+        ///DEBUG
+        Debug.Log("Ego1Actor: Phase change!");
+        ///DEBUG
+        this.gameObject.SendMessage(
+            "NextPhase", 
+            new System.Tuple<Actor, System.Action<Actor>>(
+                this, 
+                new System.Action<Actor>(InitializePhase)
+            )
+        );
+    }
+}
