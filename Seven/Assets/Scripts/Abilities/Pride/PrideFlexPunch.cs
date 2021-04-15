bd1106786bafd6d61d1a677bf11699447c10f15a
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-04-15 16:51:07 -0700

    Pride Pieces Updating
    Updated PrideCharge to include a "stop short" value, so that charges can be set to end right in front of their targets.
    
    Changed PrideFlexPunch to use the updated WeaponAbility framework, notably the inherit reference to the ability's user.

--- Seven/Assets/Scripts/Abilities/Pride/PrideFlexPunch.cs
+++ Seven/Assets/Scripts/Abilities/Pride/PrideFlexPunch.cs
@@ -8,7 +8,7 @@ public class PrideFlexPunch : WeaponAbility
     //Calls the base class internal invoke passing in the user actor. Locks the movement of the user.
     protected override int InternInvoke(params Actor[] args)
     {
-        StartCoroutine(args[0].myMovement.LockActorMovement(duration));
+        StartCoroutine(user.myMovement.LockActorMovement(duration));
         base.InternInvoke(args);
         return 0;
     }
