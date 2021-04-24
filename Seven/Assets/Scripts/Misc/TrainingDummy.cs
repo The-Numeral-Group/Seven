4d5a65c4d0c5c403fd4bed13cae1eb27e22c404e
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-04-24 16:18:31 -0700

    Added Training Dummy for easy weapon testing

---
+++ Seven/Assets/Scripts/Misc/TrainingDummy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ActorHealth))]
+public class TrainingDummy : MonoBehaviour
+{
+    ActorHealth health;
+
+    void Start()
+    {
+        health = this.gameObject.GetComponent<ActorHealth>();
+    }
+    public void DoActorDamageEffect(float damage)
+    {
+        Debug.Log($"TrainingDummy: took {damage} damage");
+    }
+
+    public void DoActorDeath()
+    {
+        Debug.Log("TrainingDummy: is slain!");
+        health.currentHealth = health.startingMaxHealth;
+    }
+}
