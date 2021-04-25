c607fce5e2df5696b760ce12456722c088e17c1d
Mingun Cho <mingun0108@gmail.com>
2021-04-24 18:35:00 -0700

    Opening Ceremony scene has been implemented

---
+++ Seven/Assets/Scripts/Misc/MayorNPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayorNPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.gameObject.GetComponent<ActiveSpeaker>().npcMode == false)
+        {
+            MenuManager.DIALOGUE_MENU.StartDialogue(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Seven/Assets/Scripts/Misc/TutorialEventListener.cs
+++ Seven/Assets/Scripts/Misc/TutorialEventListener.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialEventListener : MonoBehaviour
 {
     public GameObject gate;
+
+    public bool objectSwitch;
+
+    public bool sceneTransition;
+
+    public string sceneToLoad;
+
+    public bool mayorDialogue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Only collide with player
         if (collision.gameObject.tag == "Player")
         {
-            // close gate
-            gate.SetActive(true);
+            if(objectSwitch)
+            {
+                // close gate
+                gate.SetActive(true);
 
-            // one time use
-            this.gameObject.SetActive(false);
+                // one time use
+                this.gameObject.SetActive(false);
+                return;
+            }
+            if (sceneTransition)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+                return;
+            }
+            if (mayorDialogue)
+            {
+                var mayorObject = GameObject.Find("Mayor");
+                MenuManager.DIALOGUE_MENU.StartDialogue(mayorObject);
+            }
         }
     }
 }
