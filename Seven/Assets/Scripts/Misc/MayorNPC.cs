1216e5bddd1f932a89676781ba37a93249e7bf46
Mingun Cho <mingun0108@gmail.com>
2021-04-25 19:37:13 -0700

    Added new opening scene with cutscenes.

--- Seven/Assets/Scripts/Misc/MayorNPC.cs
+++ Seven/Assets/Scripts/Misc/MayorNPC.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class MayorNPC : MonoBehaviour
 {
+    private int nextDialogueNum = 2;
+
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(startDialogueDelay());
+    }
+
+    private IEnumerator startDialogueDelay()
+    {
+        yield return new WaitForSeconds(0.1f);
         if (this.gameObject.GetComponent<ActiveSpeaker>().npcMode == false)
         {
             MenuManager.DIALOGUE_MENU.StartDialogue(this.gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void playNextDialogue()
     {
-        
+        this.gameObject.GetComponent<ActiveSpeaker>().yarnStartNode = "Mayor.Opening.Dialogue" + nextDialogueNum;
+        MenuManager.DIALOGUE_MENU.StartDialogue(this.gameObject);
+        nextDialogueNum++;
     }
 }
