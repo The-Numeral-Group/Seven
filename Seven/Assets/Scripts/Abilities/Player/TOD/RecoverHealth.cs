87a0ed847514b0e11f6ec157c64354d5ac615caa
ramgmtk <ramgmtk@gmail.com>
2021-04-26 20:59:43 -0700

    Added indulgence player abilities

---
+++ Seven/Assets/Scripts/Abilities/Player/TOD/RecoverHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoverHealth : ActorAbilityFunction<Actor, int>
+{
+    public float healAmount = 3f;
+    public override void Invoke(ref Actor user)
+    {
+        this.user = user;
+        //by default, Invoke just does InternInvoke with no arguments
+        if(usable && user.myHealth.currentHealth < user.myHealth.maxHealth)
+        {
+            isFinished = false;
+            InternInvoke(new Actor[0]);
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+    }
+
+    public override void Invoke(ref Actor user, params object[] args)
+    {
+        Invoke(ref user);
+    }
+
+    protected override int InternInvoke(params Actor[] args)
+    {
+        user.myHealth.currentHealth = 
+            user.myHealth.currentHealth + 3 <= user.myHealth.maxHealth ?  
+            user.myHealth.currentHealth + 3 : user.myHealth.maxHealth;
+        this.isFinished = true;
+        return 0;
+    }
+}
