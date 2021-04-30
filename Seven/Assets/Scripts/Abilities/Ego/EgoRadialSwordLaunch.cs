bc4fa2c8d499b27bdcba1fcdf9751314db23547e
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-04-30 12:50:36 -0700

    New Ego Swords Draft 1
    The launcher is so different that I decided to make a separate script. It's basically just a loop with a queue that makes the swords then launches them in order.
    
    Not yet tested because I just need to  take care of some other business rn

---
+++ Seven/Assets/Scripts/Abilities/Ego/EgoRadialSwordLaunch.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EgoRadialSwordLaunch : ActorAbilityCoroutine<GameObject>
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    [Tooltip("The sword prefab to instantiate and launch.")]
+    public GameObject swordObj;
+
+    [Tooltip("How far the swords should be from the target.")]
+    public float targetOffset = 5f;
+
+    [Tooltip("How far the swords should be from each other.")]
+    public float radialOffset = 5f;
+
+    [Tooltip("How many seconds should be between each sword launch.")]
+    public float launchTimeOffset = 2f;
+
+    //METHODS--------------------------------------------------------------------------------------
+    /*Activates the ability with no arguments. In this case, it will default the target to
+    the player*/
+    public override void Invoke(ref Actor user)
+    {
+        this.user = user;
+        if(usable)
+        {
+            isFinished = false;
+            InternInvoke(GameObject.FindWithTag("Player"));
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+        
+    }
+
+    /*Same as the above method, but with a provided vector position*/
+    public override void Invoke(ref Actor user, params object[] args)
+    {
+        this.user = user;
+        //by default, Invoke just does InternInvoke with the provided arguments
+        //it's also just implicitly convert the args and give it to InternInvoke
+        if(usable)
+        {
+            isFinished = false;
+
+            //make sure the argument is a gameObject of some sort
+            if(args[0] is GameObject)
+            {
+                InternInvoke((args[0] as GameObject));
+            }
+            else if(args[0] is MonoBehaviour)
+            {
+                InternInvoke((args[0] as MonoBehaviour).gameObject);
+            }
+            else
+            {
+                Debug.LogError("EgoRadialSwordLaunch: sword launched with improper target," + 
+                    " must be GameObject or Monobehaviour");
+            }
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+    }
+
+    /*The behaviour of the swords. Place them over the target, wait a little bit, and then
+    launch them all as normal.*/
+    protected override IEnumerator InternCoroutine(params GameObject[] args)
+    {
+        //Step 0: make a queue to easily manage the swords
+        Queue<GameObject> swordQueue = new Queue<GameObject>();
+
+        //Step 0.1: Save target
+        GameObject target = args[0];
+
+        //Step 1: Generate a bunch of swords in different offset positions and add them to the
+        //queue
+        for(float rOffset = -radialOffset; rOffset <= radialOffset; rOffset += radialOffset)
+        {
+            //Step 1.1: calculate sword position
+            var swordPos = new Vector3
+            (
+                target.transform.position.x + rOffset,
+                target.transform.position.y + targetOffset,
+                target.transform.position.z + rOffset
+            );
+
+            //Step 1.2: calculate sword direction
+            var swordDir = (target.transform.position - swordPos).normalized;
+
+            //Step 1.3: Create the sword and enqueue it
+            swordQueue.Enqueue(Instantiate
+            (
+                swordObj,
+                swordPos,
+                Quaternion.Euler(swordDir.x, swordDir.y, swordDir.z),
+                target.transform
+            ));
+
+            //That makes one sword. This loop will run 3 times
+        }
+
+        //Step 2: Launch the swords
+        while(swordQueue.Count != 0)
+        {
+            //Step 2.2: wait a little bit
+            yield return new WaitForSeconds(launchTimeOffset);
+
+            //Step 2.3: get a sword to launch
+            var sword = swordQueue.Dequeue();
+
+            //Step 2.4: de-parent it
+            sword.transform.parent = null;
+
+            //Step 2.5: launch it at the target
+            EgoSwordActor swordActor;
+            if(sword.TryGetComponent(out swordActor))
+            {
+                swordActor.Launch(target);
+            }
+            else
+            {
+                Debug.LogError("EgoRadialSwordLaunch: swords are missing their EgoSwordActor" + 
+                    " components. Swords cannot be launched");
+                Destroy(sword);
+            }
+
+            //It is an error if swordObj does not have an EgoSwordActor
+        }
+
+        //By the time we get here, the method is done!
+    }
+}
