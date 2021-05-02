9a048af97addb8bff921bc1fb4c616340545c0b9
ramgmtk <ramgmtk@gmail.com>
2021-05-01 18:16:47 -0700

    Ability interactable and menu changes

--- Seven/Assets/Scripts/UI/AbilityMenu.cs
+++ Seven/Assets/Scripts/UI/AbilityMenu.cs
@@ -11,20 +11,49 @@ public class AbilityMenu : BaseUI
     public PlayerAbilityInitiator player;
     [SerializeField]
     RectTransform abilityHighLightIndicator;
+    List<(int, int)> gameSaveAbilityIndices;
+    public GameSaveManager gameSaveManager;
 
     protected override void Awake()
     {
         base.Awake();
+        gameSaveAbilityIndices = new List<(int, int)>();
         pointerToCurrentSelectedButton = 0;
+        //First int is if the ability has been picked up, the second is if the sin was comitted
+        //Indulgence flags in save manager 
+        gameSaveAbilityIndices.Add((15, 14));
+        //Ego flags in save Manager
+        gameSaveAbilityIndices.Add((15, 14));
+        //Apathy flags in save manager
+        gameSaveAbilityIndices.Add((15, 14));
     }
 
+    //Fetches player reference
     void Start()
     {
         SetupPlayerReference();
         abilityHighLightIndicator.position = abilityButtons[pointerToCurrentSelectedButton].transform.position;
+        if (gameSaveManager)
+        {
+            UnlockAbilities();
+        }
         //Temporary setup
         abilityButtons[0].SetSelectedAbility(true);
         abilityButtons[1].SetSelectedAbility(false);
+    }
+
+    //Unlocks abilities in ability buttons.
+    public void UnlockAbilities()
+    {
+        int i = 0;
+        foreach(var indexTuple in gameSaveAbilityIndices)
+        {
+            if (gameSaveManager.getBoolValue(indexTuple.Item1))
+            {
+                abilityButtons[i].SetSelectedAbility(gameSaveManager.getBoolValue(indexTuple.Item2));
+            }
+            i++;
+        }
     }
 
     void SetupPlayerReference()
