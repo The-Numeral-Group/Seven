c4a5eda092978b7634fea480cffcc1c375a3439b
Mingun Cho <mingun0108@gmail.com>
2021-05-04 18:08:37 -0700

    Added HubDoorTransition that checks for Boss's opening cutscene.

---
+++ Seven/Assets/Scripts/Interactables/HubDoorTransition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubDoorTransition : Interactable
+{
+    [Tooltip("The ID of the bossOpening ScriptableObject.")]
+    public int bossID;
+
+    //Name of the scene that should be loaded when interacting with this object.
+    [Tooltip("The name of the scene to be loaded.")]
+    public string sceneToLoad = "";
+
+    public GameObject gameSaveManager;
+
+    private GameSaveManager gameSaveManagerScript;
+
+    private void Start()
+    {
+        this.gameSaveManagerScript = gameSaveManager.GetComponent<GameSaveManager>();
+    }
+
+
+    public override void OnInteract()
+    {
+        if(bossID == 0)
+        {
+            Debug.LogWarning("SceneTranstion: No bossID provided for object " + this.gameObject.name);
+            return;
+        }
+        if (sceneToLoad == "")
+        {
+            Debug.LogWarning("SceneTranstion: No scene name provided for object " + this.gameObject.name);
+            return;
+        }
+
+        // This will be used once opening cutscene has been implemented.
+        /*if (this.gameSaveManagerScript.getBoolValue(bossID))
+        {
+            // Load Opening Cutscene
+        }
+        else
+        {
+            
+        }*/
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
