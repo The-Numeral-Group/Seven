bbf7096598dd396e8a8c328ade6747962ef0b090
Thomas Applewhite <thomashapplewhite@gmail.com>
2021-05-08 11:25:20 -0700

    Fixed Player destroying Apathy Projectiles
    This was done by creating new collision logic.
    
    Originally, the projectiles would destroy on hit with any collider, including the player's hitboxes. Now, they use FilterProjectile to only collide with one game object at a time.
    
    Also moved a bunch of hitbox-related scripts into their own folder: Misc/Hitboxes

---
+++ Seven/Assets/Scripts/Misc/Hitboxes/FilterProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterProjectile : BasicProjectile
+{
+    //FIELDS---------------------------------------------------------------------------------------
+    //the gameobject this projectile will try to collide with. All others will be ignored
+    private GameObject targetObj = null;
+
+    //METHODS--------------------------------------------------------------------------------------
+    //What happens when the projectile actually hits something
+    //ignore the collision if what was hit wasn't the target
+    protected override void OnTriggerEnter2D(Collider2D collided)
+    {
+        if(!targetObj || targetObj == collided.gameObject)
+        {
+            base.OnTriggerEnter2D(collided);
+        }
+    }
+
+    /*Starts the projectile!
+    Also saves a target, if any. This object will only collide with that target*/
+    public virtual void Launch(Vector2 target, LAUNCH_MODE mode = LAUNCH_MODE.POINT,
+        GameObject targetObj = null)
+    {
+        this.targetObj = targetObj;
+
+        base.Launch(target, mode);
+    }
+}
