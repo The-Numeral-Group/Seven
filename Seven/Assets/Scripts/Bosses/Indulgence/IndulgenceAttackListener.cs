1d001c16b62194540fb41c1e42d604c1d32b9622
Mingun Cho <mingun0108@gmail.com>
2021-05-14 23:12:41 -0700

    Updated Indulgence Dialogue, Indulgence Opening Scene

---
+++ Seven/Assets/Scripts/Bosses/Indulgence/IndulgenceAttackListener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndulgenceAttackListener : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.tag == "Player Weapon")
+        {
+            this.gameObject.transform.parent.gameObject.GetComponent<IndulgenceOpening>().nextDialogue();
+        }
+    }
+}
--- Seven/Assets/Scripts/Bosses/Indulgence/IndulgenceNPC.cs
+++ Seven/Assets/Scripts/Bosses/Indulgence/IndulgenceNPC.cs
@@ -7,6 +7,12 @@ public class IndulgenceNPC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(startDialogueDelay());
+    }
+
+    private IEnumerator startDialogueDelay()
+    {
+        yield return new WaitForSeconds(0.1f);
         if (this.gameObject.GetComponent<ActiveSpeaker>().npcMode == false)
         {
             MenuManager.DIALOGUE_MENU.StartDialogue(this.gameObject);
