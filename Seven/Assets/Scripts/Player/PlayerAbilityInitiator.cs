1ae450630fb8d6027b56504e75d2260d80fbd84e
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-05-15 19:23:37 -0700

    PlayerAbilityVFX Draft 1
    A private class inside of PlayerAbility to handle VFX that occur specifically when the player uses an ability. Currently, it's only programmed to swap the player's material while they have an ability active.
    
    The class hasn't been tested, but it is never instantiated and never referenced, so that's not a problem right now. Appropriate testing will be done once that is the case.

--- Seven/Assets/Scripts/Player/PlayerAbilityInitiator.cs
+++ Seven/Assets/Scripts/Player/PlayerAbilityInitiator.cs
@@ -21,6 +21,9 @@ public class PlayerAbilityInitiator : ActorAbilityInitiator
     to change how the player interacts*/
     public ActorAbility playerInteract;
 
+    //internal VFX manager
+    private PlayerAbilityVFX VFXManager;
+
     void Awake()
     {
         //Manual initialization, 'cause I (Thomas) have come to realize it can't be done automatically
@@ -39,6 +42,9 @@ public class PlayerAbilityInitiator : ActorAbilityInitiator
 
         this.abilities.Add("" + nameof(playerInteract), playerInteract);
         AbilityRegister.PLAYER_INTERACT = "" + nameof(playerInteract);
+
+        //also construct a VFX manager
+        //VFXManager = new PlayerAbilityVFX(this);
     }
 
     //Don't know if this is needed, but just using the player actor to pass by ref to the invoke for attack and dodge.
@@ -108,4 +114,93 @@ public class PlayerAbilityInitiator : ActorAbilityInitiator
             MenuManager.ABILITY_MENU.SelectRightAbility();
         }
     }
+
+    //private class used for messing with the player's material on ability activation
+    private class PlayerAbilityVFX
+    {
+        //FIELDS-----------------------------------------------------------------------------------
+        //the current still-happening abilities, according to DoAbilityMaterial
+        //this list will be used as a queue internally
+        private List<ActorAbility> currentAbilities;
+
+        //the current material timer, if any. Might be null.
+        private Coroutine currentAbilityTimer;
+
+        //the player's spriteRender at construction time
+        private SpriteRenderer renderer;
+
+        //the Player's AbilityInitiator to run the Coroutine on
+        private PlayerAbilityInitiator PAI;
+
+        //the special material to put on the player
+        private Material specialMat;
+
+        //the player's material the last time MaterialSwap was started
+        private Material originalMat;
+
+        //CONSTRUCTORS-----------------------------------------------------------------------------
+        public PlayerAbilityVFX(PlayerAbilityInitiator PAI, Material specialMat)
+        {
+            this.PAI = PAI;
+            this.renderer = PAI.gameObject.GetComponent<SpriteRenderer>();
+            this.specialMat = specialMat;
+
+            currentAbilities = new List<ActorAbility>();
+        }
+
+        //METHODS----------------------------------------------------------------------------------
+        /*saves the passed ability as the lastAbilityUsed, then starts a new MaterialSwap, if one
+        isn't active.*/
+        public void DoAbilityMaterial(ActorAbility ability)
+        {
+            //add this ability to the queue
+            currentAbilities.Add(ability);
+
+            //if a timer isn't currently going, make it go!
+            if(currentAbilityTimer == null) {PAI.StartCoroutine(MaterialSwap());}
+        }
+
+        /*Swaps out the player's material for something else as long as the last ability used isn't
+        finished. It should still*/
+        IEnumerator MaterialSwap()
+        {
+            //swap the materials
+            originalMat = renderer.material;
+            renderer.material = specialMat;
+
+            yield return new WaitUntil( () => 
+            {
+                //until the queue is empty...
+                while(currentAbilities.Count != 0)
+                {
+                    //check if the head is finished
+                    if(currentAbilities[0].getIsFinished())
+                    {
+                        //if so, remove it
+                        currentAbilities.RemoveAt(0);
+                    }
+                    else
+                    {
+                        //if not, return false. It won't be finished until at least the next frame
+                        return false;
+                    }
+                }
+
+                /*If the method has gotten here, that means that the queue as been emptied, and the
+                player hasn't used any new abilities since the last time one was started. So, 
+                the routine can stop waiting*/
+                return true;
+            } );
+
+            //swap the materials back
+            renderer.material = originalMat;
+
+            //and clear the timer reference
+            currentAbilityTimer = null;
+        }
+
+
+    }
 }
+
+
