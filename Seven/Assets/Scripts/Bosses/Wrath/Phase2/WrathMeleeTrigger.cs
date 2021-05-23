0a6ebc40b98308c8034c4b7da1d491abccc87c9e
ramgmtk <ramgmtk@gmail.com>
2021-05-23 01:22:06 -0700

    Setting up Wrathp2 actor script

---
+++ Seven/Assets/Scripts/Bosses/Wrath/Phase2/WrathMeleeTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+//Class is used to let wrath phase 2 know they should perform the melee attack.
+public class WrathMeleeTrigger : MonoBehaviour
+{
+    //If the player enters the range we set the flag
+    protected void OnTrigger2DEnter(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            WrathP2Actor.targetInRange = true;
+        }
+    }
+
+    //if the player leaves the range we set the flag
+    protected void OnTrigger2DExit(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            WrathP2Actor.targetInRange = true;
+        }
+    }
+}
---
+++ Seven/Assets/Scripts/Bosses/Wrath/Phase2/WrathP2Actor.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrathP2Actor : Actor
+{
+    //Static bool used to determine if the target is in range for the melee attack.
+    public static bool targetInRange;
+    //Static float used to determine if the speed at which wrath's abilities get executed.
+    //Meant to be utilized by the abilities.
+    public static float abilitySpeedMultiplier {get; private set;}
+    //Static float used to as additional damage for actor abilities
+    public static int abilityDamageAddition {get; private set;}
+    //Affects the delay between ability casts
+    [Tooltip("The amount of time Wrath waits between attacks.")]
+    [Range(0f, 10f)]
+    public float delayBetweenAttacks = 1f;
+    //At which changes in percentage differenced do we change phases. Used to increment the phasechangeactual value
+    [Tooltip("At which percentage differences do we change phases? I.e. if .25, the boss will change phases every 25%.")]
+    [Range(0.1f, 0.5f)]
+    public float phaseChangePercentageInspector = .25f;
+    //Reference to the current state
+    [Tooltip("The current state the actor is in. Meant to just be inspector viewable. Changint the value in inspector will do nothing.")]
+    public State currState;
+    public enum State
+    {
+        WAITING,
+        DEAD,
+        PHYSICAL,
+    }
+    //Rerference to the statemachine coroutine
+    IEnumerator StateMachinePTR;
+    //Reference to this instance of the class
+    Actor self;
+    //Reference to the target
+    Actor target;
+    //Reference to the currentAbility
+    ActorAbility currAbility;
+    //flag to let the actor know they have 'died'
+    bool isDead;
+    //Initialized by the inspector version. I set it up so that we avoid situations where editing the phase change value in inspector messes something up.
+    float phaseChangePercentageActual;
+
+    //Initialize member variables
+    void Awake()
+    {
+        self = this;
+        WrathP2Actor.targetInRange = false;
+        WrathP2Actor.abilitySpeedMultiplier = 1f;
+        WrathP2Actor.abilityDamageAddition = 0;
+        isDead = false;
+        phaseChangePercentageActual = phaseChangePercentageInspector;
+    }
+
+    //Setup reference to the target
+    protected override void Start()
+    {
+        base.Start();
+        SetupTarget();
+        //Start the state machine
+        StateMachinePTR = StateMachine();
+        StartCoroutine(StateMachinePTR);
+    }
+
+    //Do Actor death function
+    public override void DoActorDeath()
+    {
+        base.DoActorDeath();
+        //We set isdead so that the state machine no longer reloops on its next evaluation.
+        isDead = true;
+    }
+
+    //Function is used to setup the reference to whomever wrath is targeting. i.e. the player
+    public void SetupTarget()
+    {
+        if (target == null)
+        {
+            var playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+            {
+                target = playerObject.GetComponent<Actor>();
+            }
+            else
+            {
+                Debug.LogWarning("IndulgenceP1Actor: unable to find target for indulgence.");
+            } 
+        }
+    }
+
+    //Determine what the actors next state will be. Treating it like a behaviour tree of if else's
+    //EvaluateState is called from the statemachine coroutine.
+    void EvalauteState()
+    {
+        State decidingState = currState;
+        if(isDead) //Death is used to kill the state machine.
+        {
+            decidingState = State.DEAD;
+        }
+        else if (targetInRange)
+        {
+            decidingState = State.PHYSICAL;
+        }
+        else
+        {
+            decidingState = State.WAITING;
+        }
+        /*After checking the other states we do a health evaluation.
+        We perform this just in case we want an ability to happen on phase change
+        It is separate from the if-else chain above so that it can override w/e state decision
+        was made by the if-else checks.*/
+        if(EvaluateHealth())
+        {
+            //some state change
+        }
+        ExecuteState(decidingState);
+    }
+
+    //Called from evaluate state
+    void ExecuteState(State state)
+    {
+        switch(state)
+        {
+            case State.PHYSICAL:
+                currState = State.PHYSICAL;
+                break;
+            case State.WAITING:
+                currState = State.WAITING;
+                currAbility = null;
+                break;
+            case State.DEAD: //Dead state is used to kill the state machine
+                return;
+            default:
+                Debug.Log("Wrathp2Actor: No longer inside the state machine.");
+                break;
+        }
+        //rerun the state machine
+        StateMachinePTR = StateMachine();
+        StartCoroutine(StateMachinePTR);
+    }
+
+    //Used to determine the phases we are in for the wrath p2 fight.
+    //Called from within evaluate state.
+    //returns true pn phase change, false otherwise.
+    bool EvaluateHealth()
+    {
+        if (this.myHealth.currentHealth < this.myHealth.maxHealth * (1-phaseChangePercentageActual))
+        {
+            //every 2 phase changes we bump up the damage
+            WrathP2Actor.abilityDamageAddition = ((int)(phaseChangePercentageActual / phaseChangePercentageInspector)) / 2;
+            //Every phase change we increase the speed
+            WrathP2Actor.abilitySpeedMultiplier += 0.5f;
+            //We increase what the value for which the next phase should be evaluated at.
+            phaseChangePercentageActual += phaseChangePercentageInspector;
+            return true;
+        }
+        return false;
+    }
+
+    /*Instead of using fixedupdate, I am using a coroutine to handle the statemachine. This gives me a bit of flexibility in
+    controlling when things are executed*/
+    IEnumerator StateMachine()
+    {
+        yield return new WaitForFixedUpdate();
+        yield return new WaitUntil(()=> currAbility == null || currAbility.getIsFinished());
+        if (currState != State.WAITING)
+        {
+            yield return new WaitForSeconds(delayBetweenAttacks);
+        }
+        EvalauteState();
+    }
+}
