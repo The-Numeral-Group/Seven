4ea2adfb0e65360c2a26188ba76169f315485f50
ramgmtk <ramgmtk@gmail.com>
2021-05-30 17:02:00 -0700

    Pause menu updates and ingulence scene tweaks

---
+++ Seven/Assets/Scripts/UI/AbilitySubMenu.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+//sub menu used to handle displaying ability information text
+public class AbilitySubMenu : SubMenu
+{
+    public string lockedAbilityText = "Ability not yet acquired.";
+    public string indulgenceTODText = "";
+    public string indulgenceAOSText = "";
+    public string egoTODText = "";
+    public string egoAOSText = "";
+    public string apathyTODText = "";
+    public string apathyAOSText = "";
+    public Text infoBox;
+    public Button indulgenceButton;
+    public Button egoButton;
+    public Button apathyButton;
+    public GameSaveManager gameSaveManager;
+    Dictionary<string, (string, string)> abilityInfo;
+    Dictionary<string, (int, int)> abilityFlag;
+
+    //gamesaveindices 7, 8, 9 ability pickup :: 17, 14, 11 sin committed
+
+    void Awake()
+    {
+        infoBox.text = "";
+        abilityInfo = new Dictionary<string, (string, string)>();
+        abilityFlag = new Dictionary<string, (int, int)>();
+        abilityInfo.Add("Indulgence", (indulgenceTODText, indulgenceAOSText));
+        abilityInfo.Add("Ego", (egoTODText, egoAOSText));
+        abilityInfo.Add("Apathy", (apathyTODText, apathyAOSText));
+        abilityFlag.Add("Indulgence", (7, 17));
+        abilityFlag.Add("Ego", (8, 14));
+        abilityFlag.Add("Apathy", (9, 11));
+    }
+
+    void Start()
+    {
+        if (gameSaveManager == null)
+        {
+            var gsm = GameObject.FindObjectOfType<GameSaveManager>();
+            if (gsm)
+            {
+                gameSaveManager = gsm.GetComponent<GameSaveManager>();
+            }
+        }
+    }
+
+    public void UpdateAbilityText(string bossName)
+    {
+        if (abilityInfo.ContainsKey(bossName))
+        {
+            if (gameSaveManager.getBoolValue(abilityFlag[bossName].Item1)) //check if ability picked up
+            {
+                if (gameSaveManager.getBoolValue(abilityFlag[bossName].Item2)) //check if sin commit
+                {
+                    infoBox.text = abilityInfo[bossName].Item2;
+                }
+                else
+                {
+                    infoBox.text = abilityInfo[bossName].Item1;
+                }
+            }
+            else
+            {
+                infoBox.text = lockedAbilityText;
+            }
+        }
+        else
+        {
+            infoBox.text = "";
+        }
+    }
+}
