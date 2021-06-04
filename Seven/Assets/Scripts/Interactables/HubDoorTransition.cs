869fb6a29f516c13485c3fe3fa9dded9b307152e
Mingun Cho <mingun0108@gmail.com>
2021-06-04 15:04:52 -0700

    Made Hub Doors no longer interactble

--- Seven/Assets/Scripts/Interactables/HubDoorTransition.cs
+++ Seven/Assets/Scripts/Interactables/HubDoorTransition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class HubDoorTransition : Interactable
+public class HubDoorTransition : MonoBehaviour
 {
     [Tooltip("The ID of the bossOpening ScriptableObject.")]
     public int bossID;
@@ -21,10 +21,52 @@ public class HubDoorTransition : Interactable
     private void Start()
     {
         this.gameSaveManagerScript = gameSaveManager.GetComponent<GameSaveManager>();
+        checkParticleEffects();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (bossID == 0)
+        {
+            Debug.LogWarning("SceneTranstion: No bossID provided for object " + this.gameObject.name);
+            return;
+        }
+        if (openingScene == "")
+        {
+            Debug.LogWarning("SceneTranstion: No opening scene name provided for object " + this.gameObject.name);
+            return;
+        }
+        if (retryScene == "")
+        {
+            Debug.LogWarning("SceneTranstion: No retry scene name provided for object " + this.gameObject.name);
+            return;
+        }
+        if (!this.gameSaveManagerScript.getBoolValue(bossDefeatedIndex))
+        {
+            // This will be used once opening cutscene has been implemented.
+            if (this.gameSaveManagerScript.getBoolValue(bossID))
+            {
+                GameSettings.SCENE_TO_LOAD = openingScene;
+            }
+            else
+            {
+                GameSettings.SCENE_TO_LOAD = retryScene;
+            }
+        }
+        else
+        {
+            GameSettings.SCENE_TO_LOAD = postFightScene;
+        }
+        SceneManager.LoadScene("LoadScreen");
+    }
+
+    private void checkParticleEffects()
+    {
+
     }
 
 
-    public override void OnInteract()
+    /*public override void OnInteract()
     {
         if(bossID == 0)
         {
@@ -59,5 +101,5 @@ public class HubDoorTransition : Interactable
             GameSettings.SCENE_TO_LOAD = postFightScene;
         }
         SceneManager.LoadScene("LoadScreen");
-    }
+    }*/
 }
