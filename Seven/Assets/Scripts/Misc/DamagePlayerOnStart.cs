85146b1c2c1cab0038a0e12939f3da2ca6001105
ramgmtk <ramgmtk@gmail.com>
2021-06-06 13:38:13 -0700

    Player has less than normal health in indulgence pickup scene on no sin corrupt

---
+++ Seven/Assets/Scripts/Misc/DamagePlayerOnStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayerOnStart : MonoBehaviour
+{
+    public GameSaveManager gsm;
+    public ActorHealth playerHealth;
+    public int damageToApply = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if sin committed
+        if (!gsm.getBoolValue(17))
+        {
+            playerHealth.currentHealth = playerHealth.currentHealth > damageToApply ? playerHealth.currentHealth - damageToApply : 0;
+        }
+    }
+
+}
