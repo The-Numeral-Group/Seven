d72254a4e5254db91702f206f73f166f9c44f7a2
Thomas Applewhite <thomashapplewhite@gmail.com>
2021-06-06 18:57:38 -0700

    Switched Reset UI on Main Menu to use fonted sprites

---
+++ Seven/Assets/Scripts/UI/ButtonImageChange.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonImageChange : MonoBehaviour
+{   [Tooltip("The image to display on click with no save file")]
+    public Sprite failImage;
+
+    [Tooltip("The GameSaveManager to check for save files in")]
+    public GameObject GameSaveManager;
+
+    [Tooltip("The length of time the image should last")]
+    public float switch_duration;
+
+    //the object's Image renderer
+    private Image render;
+
+    //the original Image of the object
+    private Sprite origImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = this.gameObject.GetComponent<Image>();
+        origImage = render.sprite;
+    }
+
+    public void ImageChangePerm(Sprite newImage)
+    {
+        render.sprite = newImage;
+        origImage = newImage;
+    }
+
+    public void ImageChangeTemp(Sprite newImage)
+    {
+        StartCoroutine(changeImageDuration(newImage));
+    }
+
+    private IEnumerator changeImageDuration(Sprite newImage)
+    {
+        render.sprite = newImage;
+        yield return new WaitForSeconds(switch_duration);
+        render.sprite = origImage;
+    }
+
+    public void checkSaveFileExists()
+    {
+        if (!File.Exists(Application.persistentDataPath + ("/SaveFile.dat")))
+        {
+            this.ImageChangeTemp(failImage);
+        }
+    }
+}
